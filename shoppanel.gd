6c494221646505c1a599b6523f192c78b7427f36
Solon <walter.sobreira23@nave.org.br>
2025-03-28 19:18:28 -0300

    fixed anims, buttons still not working

--- shoppanel.gd
+++ shoppanel.gd
@@ -2,21 +2,14 @@ extends Control
 
 @onready var anim = $AnimationPlayer
 
-var fadein_playing = false
-var fadeout_playing = false
-var fadein_queued = false
-var fadeout_queued = false
+var tab_opened = false
 
 func _on_fadeinthresh_area_entered(area: Area2D) -> void:
-	if area.name == "mouseclick" and fadeout_playing == false:
-		fadein_playing = true
+	if area.name == "mouseclick" and !tab_opened:
 		anim.play("shopslidein")
-	elif area.name == "mouseclick" and fadeout_playing == true:
-		fadein_queued = true
+		tab_opened = true
 
 func _on_fadeoutthresh_area_exited(area: Area2D) -> void:
-	if area.name == "mouseclick" and fadein_playing == false:
-		fadeout_playing = true
-		anim.play("shopslidein")
-	elif area.name == "mouseclick" and fadein_playing == true:
-		fadeout_queued = true
+	if area.name == "mouseclick":
+		anim.queue("shopslideout")
+		tab_opened = false
