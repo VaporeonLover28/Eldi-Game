4d215ad24d821c30321ffe8b4c680300c8aa21bb
Wheytor <heitornasimentotavares@gmail.com>
2025-04-15 18:46:56 -0300

    small rendering fix

--- minigame_window.gd
+++ minigame_window.gd
@@ -3,15 +3,19 @@ extends SubViewportContainer
 @onready var game = $"../../../"
 @onready var minigame_handler = $"../"
 @onready var minigame_chosen : Upgrade_resource
-@onready var minigame_camera = preload("res://minigame_camera.tscn")
 @onready var texture_button = $TextureButton
 
 
 func _spawn_minigame():
-	texture_button.visible = false
+	texture_button.queue_free()
+	var minigame_subviewport_inst = SubViewport.new()
+	minigame_subviewport_inst.size = minigame_chosen.minigame_window_size
+	add_child(minigame_subviewport_inst)
 	#adding and setting minigame to the minigamewindow
 	var minigame_inst = minigame_chosen.minigame_scene.instantiate()
-	get_child(0).add_child(minigame_inst)
+	get_child(-1).add_child(minigame_inst)
 	#adding a camera to the minigame scene
-	var minigame_camera_inst = minigame_camera.instantiate()
-	get_child(0).get_child(0).add_child(minigame_camera_inst)
+	var minigame_camera_inst = Camera2D.new()
+	minigame_camera_inst.limit_left = 0
+	minigame_camera_inst.limit_top = 0
+	get_child(-1).get_child(0).add_child(minigame_camera_inst)
