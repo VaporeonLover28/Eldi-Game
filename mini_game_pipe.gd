035d98f8c6b253d9d69255ef526fe4a39debabb1
Wheytor <heitornasimentotavares@gmail.com>
2025-05-24 17:33:32 -0300

    pipe minigame logic correction
    it still is ugly

--- mini_game_pipe.gd
+++ mini_game_pipe.gd
@@ -10,14 +10,13 @@ extends Control
 
 
 @onready var correct_response_list: Array = [
+	#first element:
 	#CR = cano reto CT = cano torto
-	#if [x, false, false] = 0
-	#if [x, true, false] = 90°
-	#if [x, false, true] = 180°
-	#if [x, true, true] = 270
-	#if null is not important for the answer, 
+	#second element:
+	#null is not important for the answer,
+	#or put the degree that it need to be for the answer 
 	#make sure that any other solution are impossible as they will not count any
-	[["CR", null], ["CT", null], ["CT", null], ["CT", null], ["CT", true, false], ["CT", null], ["CR", null], ["CR", null], ["CR", null]]
+	[["CR", null], ["CT", null], ["CT", null], ["CT", null], ["CR", 90], ["CT", null], ["CR", null], ["CR", null], ["CR", null]]
 	]
 	
 @onready var current_correct_response: Array = correct_response_list.pick_random()
@@ -27,17 +26,17 @@ func _ready() -> void:
 	#for loop to decide
 	for item in grid_container.get_child_count():
 		if current_correct_response.get(item).get(0) == "CR":
-			grid_container.get_child(item).texture_normal = long_pipe_texture
+			grid_container.get_child(item).get_child(0).texture_normal = long_pipe_texture
 		else:
-			grid_container.get_child(item).texture_normal = curved_pipe_texture
+			grid_container.get_child(item).get_child(0).texture_normal = curved_pipe_texture
 			
 func _physics_process(delta: float) -> void:
 	
 	#for loop that check every frame for the correct answer decide by 
 	for item in range(grid_container.get_child_count()):
-		if(current_correct_response.get(item).get(1) == null or (current_correct_response.get(item).get(1) == grid_container.get_child(item).flip_v) and (current_correct_response.get(item).get(0) == "CR" or current_correct_response.get(item).get(2) == grid_container.get_child(item).flip_h)):
+		if(current_correct_response.get(item).get(1) == null or ((current_correct_response.get(item).get(0) == "CR" and (((current_correct_response.get(item).get(1) == 0 or current_correct_response.get(item).get(1) == 180) and (grid_container.get_child(item).get_child(0).rotation_degrees == 0 or grid_container.get_child(item).get_child(0).rotation_degrees == 180)) or ((current_correct_response.get(item).get(1) == 90 or current_correct_response.get(item).get(1) == 270) and (grid_container.get_child(item).get_child(0).rotation_degrees == 90 or grid_container.get_child(item).get_child(0).rotation_degrees == 270)))) or (current_correct_response.get(item).get(1) == grid_container.get_child(item).get_child(0).rotation_degrees))):
 			pass
 		else:
 			return
-	
-	minigame_handler.minigame_result.emit(true, minigame_window)
+	print("ganhou")
+	#minigame_handler.minigame_result.emit(true, minigame_window)
--- pipe.gd
+++ pipe.gd
@@ -9,20 +9,16 @@ func _on_pipe_pressed() -> void:
 		zero:
 			#going 90°
 			rotation_state = nine_zero
-			self.flip_v = true
-			self.flip_h = false
+			self.rotation_degrees = 90
 		nine_zero:
 			#going 180°
 			rotation_state = one_eight_zero
-			self.flip_v = false
-			self.flip_h = true
+			self.rotation_degrees = 180
 		one_eight_zero:
 			#going 270°
 			rotation_state = two_seven_zero
-			self.flip_v = true
-			self.flip_h = true
+			self.rotation_degrees = 270
 		two_seven_zero:
 			#going 0°
 			rotation_state = zero
-			self.flip_v = false
-			self.flip_h = false
+			self.rotation_degrees = 0
