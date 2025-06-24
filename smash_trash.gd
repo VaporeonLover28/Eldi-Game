15c3681509de377ff528ffaa88bee66eb5259a5f
Wheytor <161027122+Wheytor@users.noreply.github.com>
2025-06-24 15:58:15 -0300

    timing minigame done

--- smash_trash.gd
+++ smash_trash.gd
@@ -1,50 +1,54 @@
-extends Control
+extends Node2D
 @onready var minigame_handler = $"../../../"
 @onready var minigame_window = $"../../"
 @onready var timer_smash_trash: Timer = $Timer
+@onready var animation_wait_time: Timer = $Animation_wait_time
 @onready var label: Label = $Label
 @onready var color_rect: ColorRect = $ColorRect
 
+signal clicked_moused
+
 var critical_time: float = 1.0
+
 @onready var critical_points_array: Array = [
-	[1.0, 2.0, 3.0]
+	[5.0, 8.0, 2.0]
 ]
 var threshhold: float = 0.2
-var animation_wait_time : bool = true
 @onready var critical_points_chosen: Array = critical_points_array.pick_random()
 var times_smashed_correctly : int = 0
-var times_to_smash : int = critical_points_chosen.size()
+var  times_smashed : int = 0
+@onready var times_to_smash : int = critical_points_chosen.size()
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
-	_start_time_smash()
+	timer_smash_trash.start(critical_points_chosen[0])
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#label.text = str(round(timer.time_left * 10))
-	pass
-	#if timer.time_left - threshhold < critical_time and \
-	#timer.time_left + threshhold > critical_time:
-		#color_rect.color = Color.GREEN
-	#else:
-		#color_rect.color = Color.RED
+	if timer_smash_trash.time_left - threshhold < critical_time:
+		color_rect.color = Color.GREEN
+	else:
+		color_rect.color = Color.RED
+		
+	label.text = str(timer_smash_trash.time_left)
 	
-	#if Input.is_action_just_pressed("ui_accept") and animation_wait_time:
-		#if timer_smash_trash.time_left - threshhold < critical_time and \
-		#timer_smash_trash.time_left + threshhold > critical_time:
-			#print("acertou")
-		#else:
-			#print("errou")
+	if Input.is_action_just_pressed("click"):
+		print("au")
+		_on_timed_smash()
+	
 
-func _start_time_smash():
-	for item in critical_points_chosen.size():
-		timer_smash_trash.start(critical_points_chosen[item] + threshhold + 0.1)
-		await timer_smash_trash.timeout or Input.is_action_just_pressed("ui_accept")
-		if timer_smash_trash.time_left - threshhold < critical_time:
-			times_smashed_correctly =+ 1
+func _on_timed_smash():
+	times_smashed += 1
+	animation_wait_time.start()
+	print(times_smashed)
+	if timer_smash_trash.time_left - threshhold < critical_time:
+		timer_smash_trash.start(critical_points_chosen[times_smashed])
+		print("acertou")
+	else:
+		timer_smash_trash.start(critical_points_chosen[times_smashed])
+		print("errou")
+	await animation_wait_time.timeout
+	if times_smashed == times_to_smash:
+		if times_smashed_correctly == times_to_smash:
+			minigame_handler.minigame_result.emit(true, minigame_window)
 		else:
-			print("errou")
-		if item == times_to_smash:
-			if times_smashed_correctly == times_to_smash:
-				minigame_handler.minigame_result.emit(true, minigame_window)
-			else:
-				minigame_handler.minigame_result.emit(false, minigame_window)
+			minigame_handler.minigame_result.emit(false, minigame_window)
