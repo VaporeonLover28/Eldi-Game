264bb15ba02813c3210f8960aaddbddb1dc226aa
Wheytor <heitornasimentotavares@gmail.com>
2025-08-03 23:15:23 -0300

    throwable trash update

--- throwable_trash.gd
+++ throwable_trash.gd
@@ -1,9 +1,10 @@
 extends RigidBody2D
 
 @onready var arc_line: Line2D = $"../Arc"
-@onready var mouse_position = get_viewport().get_mouse_position()
+@onready var mouse_position
 var pulling_distance
 var dir
+
 var was_throwed : bool = false
 
 func _ready() -> void:
@@ -18,23 +19,18 @@ func _physics_process(delta: float) -> void:
 
 
 func _calculating_throw(delta):
-	pulling_distance = mouse_position - self.global_position
-	dir = (mouse_position - self.global_position).normalized()
-	if mouse_position.distance_to(self.global_position) > 100:
-		pulling_distance =  dir * 500 + self.global_position
-	var calculated_velocity = ((pulling_distance * dir)/10000 * pulling_distance)
+	pulling_distance = get_viewport().get_mouse_position() - self.global_position
+	var calculated_velocity = pulling_distance * 0.75
 	arc_line.clear_points()
 	var inicial_point = self.global_position
-	for item in 50000:
+	for item in 500:
 		arc_line.add_point(inicial_point)
-		inicial_point.y += 1 * delta
+		calculated_velocity.y += 5 * delta
 		inicial_point += calculated_velocity * delta
-		if inicial_point.y > 380:
-			print("oi")
+		if inicial_point.x > 340 or inicial_point.y > 340:
 			break
 
 func _throw():
 	was_throwed = true
 	freeze = false
-	var vel = mouse_position - self.global_position
-	self.apply_impulse(vel * 2)
+	self.apply_impulse(pulling_distance * 10)
