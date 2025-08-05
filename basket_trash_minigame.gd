2dc6fac0085ef159683220994ccb3ae63f5e7d59
Wheytor <161027122+Wheytor@users.noreply.github.com>
2025-08-05 14:46:54 -0300

    basket trash done

--- basket_trash_minigame.gd
+++ basket_trash_minigame.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-#@onready var game = $"../../../../../"
-#@onready var minigame_handler = $"../../../"
-#@onready var minigame_window = $"../../"
+@onready var game = $"../../../../../"
+@onready var minigame_handler = $"../../../"
+@onready var minigame_window = $"../../"
 @onready var throwable_trash = preload("res://throwable_trash.tscn")
 @onready var winning_detection_area: Area2D = $"Winning Detection Area"
 @onready var number_of_tries : int = 3
@@ -12,17 +12,16 @@ func _ready() -> void:
 	winning_detection_area.position = Vector2(316, randi_range(0 , 250))
 
 func _on_winning_detection_area_body_entered(body: Node2D) -> void:
-	#minigame_handler.minigame_result.emit(true, minigame_window)
-	pass
+	minigame_handler.minigame_result.emit(true, minigame_window)
 
 func _on_dead_zone_body_entered(body: Node2D) -> void:
-	print("oi")
 	number_of_tries -= 1
 	if number_of_tries == 0:
-		#minigame_handler.minigame_result.emit(false, minigame_window)
-		pass
+		minigame_handler.minigame_result.emit(false, minigame_window)
 	body.queue_free()
 	var throwable_trash_inst = throwable_trash.instantiate()
 	throwable_trash_inst.position = Vector2(55, 248)
+	throwable_trash_inst.freeze = true
+	throwable_trash_inst.sleeping = true
 	self.add_child(throwable_trash_inst)
 	
--- throwable_trash.gd
+++ throwable_trash.gd
@@ -3,12 +3,10 @@ extends RigidBody2D
 @onready var arc_line: Line2D = $"../Arc"
 @onready var mouse_position
 var pulling_distance
-var dir
-
 var was_throwed : bool = false
 
 func _ready() -> void:
-	freeze = true
+	pass
 	
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("click") and was_throwed == false:
@@ -25,12 +23,13 @@ func _calculating_throw(delta):
 	var inicial_point = self.global_position
 	for item in 500:
 		arc_line.add_point(inicial_point)
-		calculated_velocity.y += 5 * delta
+		calculated_velocity.y += 10 * delta
 		inicial_point += calculated_velocity * delta
 		if inicial_point.x > 340 or inicial_point.y > 340:
 			break
 
 func _throw():
 	was_throwed = true
-	freeze = false
-	self.apply_impulse(pulling_distance * 10)
+	self.freeze = false
+	self.sleeping = false
+	self.apply_impulse(pulling_distance * 6)
