82ca8a981728c1ac3c6890503b6b04556fbb8d68
Mia <vitoria.wirz23@nave.org.br>
2025-08-10 21:43:10 -0300

    UI update
    UI agora tem textura e tá bonitão

--- cursor.gd
+++ cursor.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
 @export var FOLLOW_SPEED = 10.0
-
+@onready var anim = $cursor_anim
 func _physics_process(delta):
 	var mouse_pos = get_global_mouse_position()
 	position = position.lerp(mouse_pos, delta * FOLLOW_SPEED)
+	
+	if Input.is_action_pressed("click"):
+		anim.play("Clicking")
+	else:
+		anim.play("Not_clicking")
