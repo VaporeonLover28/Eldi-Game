8aa5fc79b7b9e94026d9e2e5e98a3ee5ebde4f6b
VaporeonLover28 <waltercarvalho2904@gmail.com>
2025-08-19 15:47:12 -0300

    mini game moused clicked

--- smash_trash.gd
+++ smash_trash.gd
@@ -4,67 +4,67 @@ extends Node2D
 @onready var timer_smash_trash: Timer = $Timer
 @onready var animation_wait_time: Timer = $Animation_wait_time
 @onready var label: Label = $Label
-
+@onready var prensa: Node2D = $prensa
 @onready var nor_trash = preload("res://rec_thrash_normal.tscn")
 @onready var cru_trash = preload("res://rec_trash_crushed.tscn")
 
 signal clicked_moused
 
-var critical_time: float = 1.0
-
 @onready var critical_points_array: Array = [
-	[4.0, 5.0, 3.0],
-	[4.5, 1.5, 1.5],
-	[5.0, 1.0, 1.0],
-	[4.0, 3.0, 2.0],
+	[4.0, 7.0, 12.0],
+	[4.5, 10, 14],
+	[5.0, 9.0, 12.0],
+	[5.0, 8.0, 13.0],
 ]
-var threshhold: float = 0.4
 @onready var critical_points_chosen: Array = critical_points_array.pick_random()
 var times_smashed_correctly : int = 0
-var times_smashed : int = 0
 @onready var times_to_smash : int = critical_points_chosen.size()
-var hittable_trash := false
-# Called when the node enters the scene tree for the first time.
-var next_trash
+var trash_in_area : Array
+
+var can_crush := true
 
 func _ready() -> void:
 	print(critical_points_chosen)
-	var spawned_pos : int = 0
-	for times in critical_points_chosen.size():
+	timer_smash_trash.start(15)
+	for time in critical_points_chosen:
 		var inst = nor_trash.instantiate()
 		inst.position.y = 269
-		inst.position.x = (critical_points_chosen[times] * 100 + 220) + spawned_pos
-		spawned_pos = inst.position.x - 220
-		print("pos: " + str(inst.position))
+		inst.position.x = time * 100 + 220 - 18
 		add_child(inst)
-	
-	timer_smash_trash.start(critical_points_chosen[0])
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("click") and can_crush:
+		crush()
 
-	label.text = str(timer_smash_trash.time_left)
+func crush():
+	can_crush = false
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT_IN)
+	tween.set_trans(Tween.TRANS_EXPO)
+	tween.tween_property(prensa, "position", Vector2.ZERO, 1)
+	tween.tween_callback(func():for trash in trash_in_area:
+		var crushed = cru_trash.instantiate()
+		times_smashed_correctly += 1
+		add_child(crushed)
+		crushed.global_position = trash.global_position
+		trash.queue_free())
+	tween.tween_callback(func():animation_wait_time.start(1))
+	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_LINEAR)
+	tween.tween_property(prensa, "position", Vector2(0, -170), 1)
+	tween.tween_callback(func():can_crush = true)
 
-	if Input.is_action_just_pressed("click"):
-		animation_wait_time.start()
-		_on_timed_smash()
-	
-	if timer_smash_trash.time_left - threshhold < critical_time:
-		hittable_trash = true
+func _on_trash_detection_body_entered(body: Node2D) -> void:
+	trash_in_area.append(body)
+
+func _on_trash_detection_body_exited(body: Node2D) -> void:
+	trash_in_area.erase(body)
+
+func _on_timer_timeout() -> void:
+	if times_smashed_correctly == times_to_smash:
+		minigame_handler.minigame_result.emit(true, minigame_window)
 	else:
-		hittable_trash = false
+		minigame_handler.minigame_result.emit(false, minigame_window)
 
-func _on_timed_smash():
-	times_smashed += 1
-	animation_wait_time.start()
-	timer_smash_trash.start(critical_points_chosen[times_smashed])
-	if times_smashed == times_to_smash:
-		if times_smashed_correctly == times_to_smash:
-			minigame_handler.minigame_result.emit(true, minigame_window)
-		else:
-			minigame_handler.minigame_result.emit(false, minigame_window)
-	if hittable_trash:
-		print("acerto")
-		timer_smash_trash.start(critical_points_chosen[times_smashed])
-	else:
-		print("erro")
-		timer_smash_trash.start(critical_points_chosen[times_smashed])
+func _on_clicked_moused() -> void:
+	get_tree().quit()
---
+++ smash_trash_2.gd
@@ -0,0 +1,101 @@
+extends Node2D
+@onready var minigame_handler = $"../../../"
+@onready var minigame_window = $"../../"
+@onready var timer_smash_trash: Timer = $Timer
+@onready var animation_wait_time: Timer = $Animation_wait_time
+@onready var label: Label = $Label
+
+@onready var nor_trash = preload("res://rec_thrash_normal.tscn")
+@onready var cru_trash = preload("res://rec_trash_crushed.tscn")
+
+signal clicked_moused
+
+var critical_time: float = 1.0
+var threshhold: float = 0.4
+var trash_velocity: float = -100.0  # pixels per second
+
+@onready var critical_points_array: Array = [
+	[4.0, 5.0, 3.0],
+	[4.5, 1.5, 1.5],
+	[5.0, 1.0, 1.0],
+	[4.0, 3.0, 2.0],
+]
+@onready var critical_points_chosen: Array = critical_points_array.pick_random()
+var times_smashed_correctly : int = 0
+var times_smashed : int = 0
+@onready var times_to_smash : int = critical_points_chosen.size()
+var hittable_trash := false
+var current_trash_index: int = 0
+var trash_instances: Array = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Critical points: ", critical_points_chosen)
+	
+	# Spawn trash cans at positions where they'll reach the press at the right time
+	var press_x_position: float = 220  # Assuming this is the press position
+	
+	for i in range(critical_points_chosen.size()):
+		var inst = nor_trash.instantiate()
+		
+		# Calculate spawn position: distance = velocity * time
+		var distance_to_travel = abs(trash_velocity) * critical_points_chosen[i]
+		inst.position = Vector2(press_x_position + distance_to_travel, 269)
+		
+		add_child(inst)
+		trash_instances.append(inst)
+		print("Trash ", i, " spawned at x: ", inst.position.x, " with time: ", critical_points_chosen[i])
+	
+	# Start timer for first trash can
+	timer_smash_trash.start(critical_points_chosen[0])
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	label.text = "Time left: %.2f" % timer_smash_trash.time_left
+	
+	# Calculate if we're in the hittable window
+	var time_until_smash = timer_smash_trash.time_left
+	hittable_trash = time_until_smash <= critical_time and time_until_smash >= (critical_time - threshhold)
+	
+	if Input.is_action_just_pressed("click"):
+		_on_timed_smash()
+
+func _on_timed_smash():
+	if hittable_trash:
+		print("Hit at correct time!")
+		times_smashed_correctly += 1
+		# Visual feedback - change trash can to crushed
+		if current_trash_index < trash_instances.size():
+			trash_instances[current_trash_index].queue_free()
+			var crushed = cru_trash.instantiate()
+			crushed.position = trash_instances[current_trash_index].position
+			add_child(crushed)
+	else:
+		print("Missed timing!")
+	
+	# Move to next trash can
+	current_trash_index += 1
+	times_smashed += 1
+	
+	if current_trash_index < critical_points_chosen.size():
+		# Start timer for next trash can with exact timing
+		timer_smash_trash.start(critical_points_chosen[current_trash_index])
+	else:
+		# All trash cans processed
+		_end_minigame()
+
+func _on_timer_timeout() -> void:
+	# Timer ran out without player input
+	print("Timeout for trash can ", current_trash_index)
+	times_smashed += 1
+	current_trash_index += 1
+	
+	if current_trash_index < critical_points_chosen.size():
+		timer_smash_trash.start(critical_points_chosen[current_trash_index])
+	else:
+		_end_minigame()
+
+func _end_minigame():
+	var success = times_smashed_correctly == times_to_smash
+	print("Game over. Correct: ", times_smashed_correctly, "/", times_to_smash)
+	minigame_handler.minigame_result.emit(success, minigame_window)
