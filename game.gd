60ff7f1415cc209600d5cb368f17f30663e32be8
VaporeonLover28 <waltercarvalho2904@gmail.com>
2025-10-07 14:29:55 -0300

    Revert "SIIIIIIIIIUUUUUUU"
    This reverts commit 0e77bfae121343b26b76a985e268322c943e23e7.

--- game.gd
+++ game.gd
@@ -20,8 +20,6 @@ var rating : float = 0.0:
 			rating = value
 		update_rating()
 
-var moneypers : int
-
 #declaring item shop
 #when adding item: click node on inspector
 #and add new element with parameters
