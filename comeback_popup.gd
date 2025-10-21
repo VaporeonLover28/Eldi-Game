48391c01c0f39ce798a361842be73834610c1ccf
VaporeonLover28 <waltercarvalho2904@gmail.com>
2025-10-21 15:35:30 -0300

    did sumthn
    - fix shop
    - add hover desc for shop items
    - lock shop items behind NR with visible lock
    - until friday

--- comeback_popup.gd
+++ comeback_popup.gd
@@ -6,10 +6,10 @@ extends PanelContainer
 
 func _ready() -> void:
 	tween = create_tween()
-	tween.tween_property(self, "position", Vector2(380, 150), 3.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
+	tween.tween_property(self, "position", Vector2(380, 150), 1.25).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 
 func _hide_comeback_popup():
 	tween = create_tween()
-	tween.tween_property(self, "position", Vector2(380, 960), 3.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
+	tween.tween_property(self, "position", Vector2(380, 960), 1.25).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 	game.money += game.money_stacked
 	game.money_stacked = 0
